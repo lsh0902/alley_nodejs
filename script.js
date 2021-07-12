9cf1c78ef0185adc6843e706e8245f2becc970e8
lsh0902 <61610978+lsh0902@users.noreply.github.com>
2021-07-12 21:20:29 +0900

    Update script.js

--- script.js
+++ script.js
@@ -6,11 +6,12 @@ fs.promises.readdir(targetDir)
   .then(res => {
     res.forEach(file => {
       const parsed = path.parse(file);
-        if('.jpg' === parsed.ext && parsed.name.includes('E'))  middleDir = 'duplicated'
-        else if(['.aae', '.png'].includes(parsed.ext))          middleDir = 'captured'
-        else if(['.mov','.mp4'].includes(parsed.ext))           middleDir = 'video'
+      if('.jpg' === parsed.ext && parsed.name.includes('E'))  middleDir = 'duplicated'
+      else if(['.aae', '.png'].includes(parsed.ext))          middleDir = 'captured'
+      else if(['.mov','.mp4'].includes(parsed.ext))           middleDir = 'video'
+      else return; 
 
-        if(!fs.existsSync(path.join(targetDir, middleDir))) fs.mkdirSync(path.join(targetDir,middleDir))
-        fs.rename(path.join(targetDir, parsed.base), path.join(targetDir,middleDir, parsed.base), (error, data) =>{console.log(error, data)})
-      })
-    }).catch(console.log);
+      if(!fs.existsSync(path.join(targetDir, middleDir))) fs.mkdirSync(path.join(targetDir,middleDir))
+      fs.rename(path.join(targetDir, parsed.base), path.join(targetDir,middleDir, parsed.base), (error, data) =>{console.log(error, data)})
+    })
+  }).catch(console.log);
