640b111c442626f2846c460eaa44cb49237a5b35
lsh0902 <61610978+lsh0902@users.noreply.github.com>
2021-07-12 21:09:50 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+targetDir = process.argv[2];
+
+fs.promises.readdir(targetDir)
+  .then(res => {
+    res.forEach(file => {
+      const parsed = path.parse(file);
+        if('.jpg' === parsed.ext && parsed.name.includes('E')) middleDir = 'duplicated'
+        else if(['.aae', '.png'].includes(parsed.ext)) middleDir = 'captured'
+        else middleDir = 'video'
+
+        if(!fs.existsSync(path.join(targetDir, middleDir))) fs.mkdirSync(path.join(targetDir,middleDir))
+        fs.rename(path.join(targetDir, parsed.base), path.join(targetDir,middleDir, parsed.base), (error, data) =>{console.log(error, data)})
+      })
+    }).catch(console.log);
